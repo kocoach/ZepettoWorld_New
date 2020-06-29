7859f2d4d3cdb8543956b9f5d2dfd13e8a218840
smyoo <smyoo@ubivelox.com>
2020-06-29 17:57:35 +0900

    First Commit
    - 프로젝트 환경 구축
    - git ignore 설정

---
+++ src/test/java/com/zepetto/world/Renew/RenewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zepetto.world.Renew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
